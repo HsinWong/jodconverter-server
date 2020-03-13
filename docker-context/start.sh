4a2c2bc6b07993fc2bae4e93077fadf9b61c067d
xinh <xinh@xinshiyun.com>
2020-03-13 16:43:21 +0800

    替换为openoffice

--- docker-context/start.sh
+++ docker-context/start.sh
@@ -3,4 +3,4 @@
 # 刷新字体缓存
 fc-cache -fv
 
-java -Djava.security.egd=file:/dev/./urandom -jar /app.jar --jodconverter.local.officeHome=/opt/libreoffice6.3 $*
+java -Djava.security.egd=file:/dev/./urandom -jar /app.jar $*
